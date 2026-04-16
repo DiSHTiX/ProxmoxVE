d8ed3140f861e52fb4c43da0820c45ea6e4b990d
DiSHTiX <26351298+DiSHTiX@users.noreply.github.com>
2026-04-17 06:24:46 +0800

    For AMD install rocm version of ollama

--- install/ollama-install.sh
+++ install/ollama-install.sh
@@ -110,7 +110,11 @@ RELEASE=$(curl -fsSL https://api.github.com/repos/ollama/ollama/releases/latest 
 OLLAMA_INSTALL_DIR="/usr/local/lib/ollama"
 BINDIR="/usr/local/bin"
 mkdir -p $OLLAMA_INSTALL_DIR
-OLLAMA_URL="https://github.com/ollama/ollama/releases/download/${RELEASE}/ollama-linux-amd64.tar.zst"
+if [[ "$GPU_BACKEND" == "rocm" ]]; then
+    OLLAMA_URL="https://github.com/ollama/ollama/releases/download/${RELEASE}/ollama-linux-amd64-rocm.tar.zst"
+  else
+    OLLAMA_URL="https://github.com/ollama/ollama/releases/download/${RELEASE}/ollama-linux-amd64.tar.zst"
+fi
 TMP_TAR="/tmp/ollama.tar.zst"
 echo -e "\n"
 if curl -fL# -C - -o "$TMP_TAR" "$OLLAMA_URL"; then
